35ccf853314e097c4c6b359ac347fa58e4a2ed6f
mrbalbin <xxbalbin@gmail.com>
2025-06-06 12:27:34 -0500

    cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/database');
 const User = require('./models/User');
 const Role = require('./models/Role');
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
